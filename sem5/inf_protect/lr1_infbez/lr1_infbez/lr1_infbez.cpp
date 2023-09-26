ced45ae26544b02c2ec0f748b61f8f2c7d94f8b9
Rustamiy_S <112976411+Rustamiys@users.noreply.github.com>
2023-09-26 19:55:08 +0300

    Add files via upload

---
+++ sem5/inf_protect/lr1_infbez/lr1_infbez/lr1_infbez.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <Windows.h>
+
+using namespace std;
+
+const int n = 256;
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	string key;
+	unsigned char word_ch;
+	unsigned char key_ch;
+	string file_name_read;
+	string file_name_load;
+	int number;
+
+	while (true) {
+		cout << "1. Кодирование" << endl << "2. Декодирование" << endl
+			 << "0. Закончить программу" << endl;
+		cout << "Выберите число : ";	
+		cin >> number;
+		cin.clear();
+		cin.ignore(1024, '\n');
+		switch (number)
+		{
+		case 0:
+			return 0;
+			//Шайхутдинов Рустам АС-21-04
+		case 1:
+		{
+			file_name_load.clear();
+			file_name_read.clear();
+			cout << "Input read filename : ";
+			getline(cin, file_name_read);
+			cout << "Input load filename : ";
+			getline(cin, file_name_load);
+			cout << "Ключ: ";
+			getline(cin, key);
+
+			ifstream ining(file_name_read, ios::binary);
+			ofstream outing(file_name_load, ios::binary);
+
+			if (ining.is_open() && outing.is_open())
+			{
+				unsigned char ch;
+				unsigned char key_ch;
+				char curr_char;
+				int cnt = 0;
+				while (!ining.eof() && ining.get(curr_char))
+				{
+					key_ch = unsigned char(key[cnt % key.size()]);
+					ch = unsigned char((n - int(key_ch) + int(unsigned char(curr_char))) % n);
+					outing << ch;
+				}
+			}
+
+			outing.close();
+			ining.close();
+			break;
+		}
+		case 2:
+		{
+			file_name_load.clear();
+			file_name_read.clear();
+			cout << "Input read filename : ";
+			getline(cin, file_name_read);
+			cout << "Input load filename : ";
+			getline(cin, file_name_load);
+			cout << "Ключ: ";
+			getline(cin, key);
+
+			ifstream ining(file_name_read, ios::binary);
+			ofstream outing(file_name_load, ios::binary);
+
+			if (ining.is_open() && outing.is_open())
+			{
+				unsigned char ch;
+				unsigned char key_ch;
+				char curr_char;
+				int cnt = 0;
+				while (!ining.eof() && ining.get(curr_char))
+				{
+					key_ch = unsigned char(key[cnt % key.size()]);
+					ch = unsigned char((int(key_ch) + int(unsigned char(curr_char))) % n);
+					outing << ch;
+				}
+			}
+
+			outing.close();
+			ining.close();
+			break;
+		}
+		default:
+			return 0;
+		}
+	}
+}
