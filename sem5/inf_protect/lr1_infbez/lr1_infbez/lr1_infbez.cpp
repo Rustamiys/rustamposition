6ffac372ba5c8a17bb1c0a26061156192099fdc3
Rustamiy_S <112976411+Rustamiys@users.noreply.github.com>
2023-10-03 12:13:24 +0300

    Update lr1_infbez.cpp

--- sem5/inf_protect/lr1_infbez/lr1_infbez/lr1_infbez.cpp
+++ sem5/inf_protect/lr1_infbez/lr1_infbez/lr1_infbez.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <fstream>
 #include <Windows.h>
@@ -21,17 +21,12 @@ int main()
 
 	while (true) {
 		cout << "1. Кодирование" << endl << "2. Декодирование" << endl
-			 << "0. Закончить программу" << endl;
-		cout << "Выберите число : ";	
+			<< "0. Закончить программу" << endl;
+		cout << "Выберите число : ";
 		cin >> number;
 		cin.clear();
 		cin.ignore(1024, '\n');
-		switch (number)
-		{
-		case 0:
-			return 0;
-			//Шайхутдинов Рустам АС-21-04
-		case 1:
+		if (number==1 || number==2)
 		{
 			file_name_load.clear();
 			file_name_read.clear();
@@ -54,48 +49,15 @@ int main()
 				while (!ining.eof() && ining.get(curr_char))
 				{
 					key_ch = unsigned char(key[cnt % key.size()]);
-					ch = unsigned char((n - int(key_ch) + int(unsigned char(curr_char))) % n);
+					ch = unsigned char((n + int(pow(-1, number))*int(key_ch) + int(unsigned char(curr_char))) % n);
 					outing << ch;
 				}
 			}
 
 			outing.close();
 			ining.close();
-			break;
 		}
-		case 2:
-		{
-			file_name_load.clear();
-			file_name_read.clear();
-			cout << "Input read filename : ";
-			getline(cin, file_name_read);
-			cout << "Input load filename : ";
-			getline(cin, file_name_load);
-			cout << "Ключ: ";
-			getline(cin, key);
-
-			ifstream ining(file_name_read, ios::binary);
-			ofstream outing(file_name_load, ios::binary);
-
-			if (ining.is_open() && outing.is_open())
-			{
-				unsigned char ch;
-				unsigned char key_ch;
-				char curr_char;
-				int cnt = 0;
-				while (!ining.eof() && ining.get(curr_char))
-				{
-					key_ch = unsigned char(key[cnt % key.size()]);
-					ch = unsigned char((int(key_ch) + int(unsigned char(curr_char))) % n);
-					outing << ch;
-				}
-			}
-
-			outing.close();
-			ining.close();
-			break;
-		}
-		default:
+		else {
 			return 0;
 		}
 	}
